5ce919f2f472610aa794578dd6afd53b81309252
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-10-23 11:11:38 -0300

    modificacoes funcionais

--- WebApplication1/WebApplication1/Scripts/Utils/scripts.js
+++ WebApplication1/WebApplication1/Scripts/Utils/scripts.js
@@ -11,75 +11,116 @@ $(document).ready(function () {
 });
 
 
+
+$("#consultar").click(function () {
+
+    var params = {
+        'nCdEmpresa': '',
+        'sDsSenha': '',
+        'sCepOrigem': '74380150',
+        'sCepDestino': '43810040',
+        'nVlPeso': '5',
+        'nCdFormato': '1',
+        'nVlComprimento': '16',
+        'nVlAltura': '5',
+        'nVlLargura': '15',
+        'nVlDiametro': '0',
+        'sCdMaoPropria': 's',
+        'nVlValorDeclarado': '200',
+        'sCdAvisoRecebimento': 'n',
+        'StrRetorno': 'xml',
+        'nCdServico': '40010'
+    };
+
+    var url = 'https://usysweb.com.br/api/correiosambev.php';
+
+    var options = {
+        'uri': url,
+        'method': 'GET',
+        'qs': params
+    };
+
+    $.getJSON(options.uri, options.qs, function (data) {
+        //debugger
+        alert(data.cServico.Valor);
+    });
+
+});
+
+
 function ClearScreen() {
     var myInputs = $('input[type="text"]');
     myInputs.val("");
 }
 
-function calcWsCorreios() {
-    var sendjson = {
-        "nCdEmpresa": "",
-        "sDsSenha": "",
-        "sCepOrigem": "37540000",
-        "sCepDestino": "37540000",
-        "nVlPeso": "1",
-        "nCdFormato": "1",
-        "nVlComprimento": "20",
-        "nVlAltura": "5",
-        "nVlLargura": "15",
-        "sCdMaoPropria": "s",
-        "nVlValorDeclarado": "200",
-        "sCdAvisoRecebimento": "s",
-        "nCdServico": "41106",
-        "nVlDiametro": "0", 
-        "StrRetorno": "xml",
-        "nIndicaCalculo" : 3
-    }
 
 
-    $.ajax({
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        url: "http://usysweb.com.br/api/correiosambev.php?",
-        data: sendjson,
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (data, err) {
-
-            console.log(data);
-            console.log(err);
-        },
-    });
-
-}
-
-$(document).ready(function () {
-    $('#teste').click(function () {
-        // calcWsCorreios();
-        $("body").append('<div id=result>Clickou</div>');
 
 
-        $.ajax({
-            type: "GET",
-            contentType: "application/json; charset=utf-8",
-            url: "http://usysweb.com.br/api/correiosambev.php?",
-            data: sendjson,
-            dataType: "json",
-            jsonpCallback: "foo"
-            , error: function (err) { alert(err); }
-            , success: 'foo'
-        }).success(function (s) {
-            console.log('Successo', s);
-        }).done(function (done) {
-            console.log('Done', done);
-        });
+//function calcWsCorreios() {
+//    var sendjson = {
+//        "nCdEmpresa": "",
+//        "sDsSenha": "",
+//        "sCepOrigem": "37540000",
+//        "sCepDestino": "37540000",
+//        "nVlPeso": "1",
+//        "nCdFormato": "1",
+//        "nVlComprimento": "20",
+//        "nVlAltura": "5",
+//        "nVlLargura": "15",
+//        "sCdMaoPropria": "s",
+//        "nVlValorDeclarado": "200",
+//        "sCdAvisoRecebimento": "s",
+//        "nCdServico": "41106",
+//        "nVlDiametro": "0", 
+//        "StrRetorno": "xml",
+//        "nIndicaCalculo" : 3
+//    }
 
-        function foo(resultado) {
-            // verifique que resultado é JSON válido
-            alert(resultado);
-            console.log('porra');
-        }
-    });//click
-});// ready
+
+//    $.ajax({
+//        type: "GET",
+//        contentType: "application/json; charset=utf-8",
+//        url: "http://usysweb.com.br/api/correiosambev.php?",
+//        data: sendjson,
+//        dataType: "json",
+//        success: function (data) {
+//            console.log(data);
+//        },
+//        error: function (data, err) {
+
+//            console.log(data);
+//            console.log(err);
+//        },
+//    });
+
+//}
+
+//$(document).ready(function () {
+//    $('#teste').click(function () {
+//        // calcWsCorreios();
+//        $("body").append('<div id=result>Clickou</div>');
+
+
+//        $.ajax({
+//            type: "GET",
+//            contentType: "application/json; charset=utf-8",
+//            url: "http://usysweb.com.br/api/correiosambev.php?",
+//            data: sendjson,
+//            dataType: "json",
+//            jsonpCallback: "foo"
+//            , error: function (err) { alert(err); }
+//            , success: 'foo'
+//        }).success(function (s) {
+//            console.log('Successo', s);
+//        }).done(function (done) {
+//            console.log('Done', done);
+//        });
+
+//        function foo(resultado) {
+//            // verifique que resultado é JSON válido
+//            alert(resultado);
+//            console.log('porra');
+//        }
+//    });//click
+//});// ready
