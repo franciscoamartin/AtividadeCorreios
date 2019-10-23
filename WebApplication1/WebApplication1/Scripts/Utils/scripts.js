cc7347ccc10e54e11f2f8496e30536bc830bb441
Anuska Kepler Rehn <anuska.rehn@gmail.com>
2019-10-23 09:08:12 -0300

    k

---
+++ WebApplication1/WebApplication1/Scripts/Utils/scripts.js
@@ -0,0 +1,20 @@
+﻿
+$('#ceporigem').mask("00000-000");
+$('#cepdestino').mask("00000-000");
+
+$(document).ready(
+    function () {
+
+
+    });
+
+
+$('#limpar').click(function () {
+    ClearScreen();
+});
+
+
+function ClearScreen() {
+    var myInputs = $('input[type="text"]');
+    myInputs.val("");
+}
