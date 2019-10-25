dcb68921bb2e1d317f9784ef021f3d8ad6646a1d
Francisco Martin <franciscoaugustomartin@gmail.com>
2019-10-25 11:29:04 -0300

    Atividade Completa via POST

--- WebApplication1/WebApplication1/Scripts/Utils/scripts.js
+++ WebApplication1/WebApplication1/Scripts/Utils/scripts.js
@@ -1,61 +1,4 @@
 ﻿
-//$(document).ready(function () {
-
-//    $('#limpar').click(function () {
-//        ClearScreen();
-//    });
-
-
-
-//});
-
-
-//$("#consultar").click(function () {
-
-//    var params = {
-//        'sCepOrigem': $('#ceporigem').val(),
-//        'sCepDestino': $('#cepdestino').val(),
-//        'nVlPeso': $('#peso').val(),
-//        'nCdFormato': $("#formatoencomenda :selected").val(),
-//        'nVlComprimento': $('#comprimento').val(),
-//        'nVlAltura': $('#altura').val(),
-//        'nVlLargura': $('#largura').val(),
-//        'nVlDiametro': $('#diametro').val(),
-//        'sCdMaoPropria': $("#entregamao :selected").val(),
-//        'nVlValorDeclarado': $('#valordeclarado').val(),
-//        'sCdAvisoRecebimento': $("#avisorecebimento :selected").val(),
-//        'StrRetorno': 'xml',
-//        'nCdServico': $("#selectservico :selected").val(),
-//    };
-
-//    var url = 'https://usysweb.com.br/api/correiosambev.php';
-
-//    var options = {
-//        'uri': url,
-//        'method': 'GET',
-//        'qs': params
-//    };
-
-//    $.getJSON(options.uri, options.qs, function (data) {
-//        //debugger
-
-//        $('#resultadoValor').val("R$ " + data.cServico.Valor);
-//        $('#resultadoPrazo').val(data.cServico.PrazoEntrega + " dia(s)");
-//        $('#resultadoSemAdicionais').val("R$ " + data.cServico.ValorSemAdicionais);
-//        //$('#resultadoEmMaos').val(data.cServico.EntregaDomiciliar);
-
-
-
-
-//        //alert(" Valor Final" + data.cServico.Valor
-//        //    + "\n\rPrazo Entrega " + data.cServico.PrazoEntrega
-//        //    + "\n\rValor Sem Adicionais " + data.cServico.ValorSemAdicionais
-//        //);
-//    });
-
-//});
-
-
 $('#ceporigem').mask("00000-000");
 $('#cepdestino').mask("00000-000");
 $('#nVlValorDeclarado').mask('#.###,00', { reverse: true });
@@ -68,18 +11,16 @@ $(document).ready(function () {
         //Obtemos o atributo que indica a pagina que vamos enviar o post
         var obterAtributo = $('#formenviar').attr('send-post');
 
-        debugger
-
+      
         //Enviamos o post para nosso servidor web
         $.post("https://usysweb.com.br/api/" + obterAtributo + ".php?giomar=true", informacoes, function (data) {
             //aqui como recebemos uma string com formato de JSON
             //temos que passar ela para o JSON parecido com o int.Parse()
             data = JSON.parse(data);
-            debugger
 
             //Aqui carregamos as informações automaticamente
             $.each(data.cServico, function (key, value) {
-                $('input[name="{key}"]'.replace("{key}", key)).val(value);
+                $('input[name="{key}"]'.replace("{key}", key)).val(value); //valor de entrada a ser susbstituida, mesmo name recebido pela resposta do server. 
             });
         });
     });
